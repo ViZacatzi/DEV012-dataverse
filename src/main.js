9ee9ee3314a6d7471e7765d755cfd3ff5f54cc1c
ViZacatzi <vizacatzi@gmail.com>
2023-10-23 11:24:19 -0600

    tarjetas visibles

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
+
+const div = document.querySelector("#root")
+
+div.innerHTML = renderItems(data)
+
